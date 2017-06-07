855963669d7b73f8c57e23cb7e939c63916326f0
baesparza <bruno.be81@gmail.com>
2017-06-07 18:32:42 -0500

    marcadores manejados por el servicio y añadir marcadores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { MapasService } from './services/mapas.service';
+import { Marcador } from './interface/marcador.interface';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,5 +13,23 @@ export class AppComponent {
   lat: number = -4.001705;
   lng: number = -79.201420;
   zoom: number = 17;
+
+
+  constructor ( public _mapasService: MapasService ) {}
+
+
+  clickMapa( event ) {
+
+    let marcador: Marcador = {
+      lat : event.coords.lat,
+      lng : event.coords.lng,
+      titulo: 'Sin titulo',
+      draggable: true
+    };
+
+    this._mapasService.insertar_marcador( marcador );
+
+    // console.log(event);
+  }
 
 }
--- src/app/services/mapas.service.ts
+++ src/app/services/mapas.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Marcador } from './../interface/marcador.interface';
 
 @Injectable()
 export class MapasService {
 
-  constructor() { }
+  marcadores: Marcador[] = [];
+
+  constructor() {
+    let nuevo_marcador: Marcador = {
+      lat: -4.001705,
+      lng: -79.201420,
+      titulo: 'Plaza de San Sebastian',
+      draggable: true
+    };
+
+    this.marcadores.push( nuevo_marcador );
+  }
+
+  insertar_marcador( marcador: Marcador ) {
+    this.marcadores.push( marcador );
+    // console.log(this.marcadores);
+  }
 
 }
